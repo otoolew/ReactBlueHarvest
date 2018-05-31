4568dfe90cd3f16d26f6d0d8734c58f8648c62ac
O'Toole <WBO5@pitt.edu>
2018-05-31 13:27:55 -0400

    redux and proptypes installed

---
+++ src/Something.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Something extends Component {
+  render() {
+    return (  
+        <h1>Welcome to React</h1>
+    );
+  }
+}
+
+export default Something;
